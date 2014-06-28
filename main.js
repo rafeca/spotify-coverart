0b54b898ecf339dca8a2295c3af5ea0992096891
José Manuel Pérez <jmperez1985@gmail.com>
2014-06-28 15:29:29 +0200

    Make sure it doesn't break if images is empty or wrong

--- main.js
+++ main.js
@@ -9,31 +9,37 @@
         interval = null;
       }
 
-      var images = JSON.parse(this.images);
-      var that = this;
+      if (this.images) {
+        try {
+          var images = JSON.parse(this.images);
+          var that = this;
 
-      // todo: once we have loaded a large one maybe it doesn't make sense to try to load
-      // a smaller one, since that incurs in an extra request and the quality won't be
-      // better than the one provided by the larger image
+          // todo: once we have loaded a large one maybe it doesn't make sense to try to load
+          // a smaller one, since that incurs in an extra request and the quality won't be
+          // better than the one provided by the larger image
 
-      var findRightImage = function() {
-        var targetWidth = that.offsetWidth * window.devicePixelRatio,
-            targetHeight = that.offsetHeight * window.devicePixelRatio;
+          var findRightImage = function() {
+            var targetWidth = that.offsetWidth * window.devicePixelRatio,
+                targetHeight = that.offsetHeight * window.devicePixelRatio;
 
-        if (targetWidth === 0 || targetHeight === 0) {
-          return;
+            if (targetWidth === 0 || targetHeight === 0) {
+              return;
+            }
+            
+            var cover = images[0].url;
+            for (var i=1; i<images.length; i++) {
+              if (images[i].width >= targetWidth && images[i].width >= targetHeight) {
+                cover = images[i].url;
+              }
+            }
+            that.cover = cover;
+          };
+          interval = setInterval(findRightImage, 200);
+          findRightImage();
+        } catch (e) {
+          console.error(e);
         }
-        
-        var cover = images[0].url;
-        for (var i=1; i<images.length; i++) {
-          if (images[i].width >= targetWidth && images[i].width >= targetHeight) {
-            cover = images[i].url;
-          }
-        }
-        that.cover = cover;
-      };
-      interval = setInterval(findRightImage, 200);
-      findRightImage();
+      }
     }
   });
 
